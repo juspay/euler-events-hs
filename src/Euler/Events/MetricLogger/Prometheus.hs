ee340a4b3653c4676f04e3a1ebae2fea96f44df6
Puneet <puneet.pahuja@juspay.in>
2020-08-07 19:40:37 +0530

    Puts Prometheus utils in a separate module

--- src/Euler/Events/MetricLogger/Prometheus.hs
+++ src/Euler/Events/MetricLogger/Prometheus.hs
@@ -3,16 +3,15 @@ module Euler.Events.MetricLogger.Prometheus where
 import           Control.Concurrent                (forkIO)
 import           Data.Functor                      (($>))
 import           Data.List                         (find)
-import qualified Data.Map                          as Map
-import           Data.Maybe                        (fromMaybe)
-import           Data.Ratio                        ((%))
 import           Data.Text                         (Text)
 import qualified Data.Text                         as T
 import           Data.Text.Encoding                (decodeUtf8)
 import           Euler.Events.Class                (MetricLogger (..))
 import           Euler.Events.Types.Metric         (MetricOperation, MetricResult)
 import qualified Euler.Events.Types.Metric         as M
-import           Euler.Events.Util                 (tshow)
+import           Euler.Events.Util.Prometheus      (incrementClientAuthTokenGenerated, incrementOrderStatusCacheAdd,
+                                                    incrementOrderStatusCacheHit, incrementOrderStatusCacheMiss,
+                                                    observeSeconds)
 import qualified Network.HTTP.Types                as HTTP
 import           Network.Wai                       (Middleware)
 import qualified Network.Wai                       as Wai
@@ -21,9 +20,7 @@ import qualified Network.Wai.Middleware.Prometheus as Prometheus
 import qualified Prometheus
 import qualified Prometheus.Metric.GHC             as Prometheus
 import qualified Prometheus.Metric.Proc            as Prometheus
-import           System.Clock                      (Clock (..), TimeSpec, diffTimeSpec, getTime, toNanoSecs)
-import           System.Environment                (getEnvironment)
-import           System.Posix.Process              (getProcessID)
+import           System.Clock                      (Clock (..), getTime)
 
 data PrometheusMetric
   = Counter Prometheus.Counter
@@ -89,107 +86,3 @@ instance MetricLogger PrometheusConfig PrometheusMetric where
              Wai.responseHeaders res)
       observeSeconds status method path merchantId start end
       respond res
-
-withPrefix :: Text -> Text
-withPrefix = ("euler_" <>)
-
-clientAuthTokenGeneratedCounter :: Prometheus.Vector Text Prometheus.Counter
-clientAuthTokenGeneratedCounter =
-  Prometheus.unsafeRegister $
-  Prometheus.vector "merchant_id" $
-  Prometheus.counter $
-  Prometheus.Info (withPrefix "client_auth_token_generated") ""
-
-orderStatusCacheHitCounter :: Prometheus.Vector Text Prometheus.Counter
-orderStatusCacheHitCounter =
-  Prometheus.unsafeRegister $
-  Prometheus.vector "merchant_id" $
-  Prometheus.counter $ Prometheus.Info (withPrefix "order_status_cache_hit") ""
-
-orderStatusCacheMissCounter :: Prometheus.Vector Text Prometheus.Counter
-orderStatusCacheMissCounter =
-  Prometheus.unsafeRegister $
-  Prometheus.vector "merchant_id" $
-  Prometheus.counter $ Prometheus.Info (withPrefix "order_status_cache_miss") ""
-
-orderStatusCacheAddCounter :: Prometheus.Vector Text Prometheus.Counter
-orderStatusCacheAddCounter =
-  Prometheus.unsafeRegister $
-  Prometheus.vector "merchant_id" $
-  Prometheus.counter $ Prometheus.Info (withPrefix "order_status_cache_add") ""
-
-incrementClientAuthTokenGenerated :: Text -> IO ()
-incrementClientAuthTokenGenerated merchantId =
-  Prometheus.withLabel
-    clientAuthTokenGeneratedCounter
-    merchantId
-    Prometheus.incCounter
-
-incrementOrderStatusCacheHit :: Text -> IO ()
-incrementOrderStatusCacheHit merchantId =
-  Prometheus.withLabel
-    orderStatusCacheHitCounter
-    merchantId
-    Prometheus.incCounter
-
-incrementOrderStatusCacheMiss :: Text -> IO ()
-incrementOrderStatusCacheMiss merchantId =
-  Prometheus.withLabel
-    orderStatusCacheMissCounter
-    merchantId
-    Prometheus.incCounter
-
-incrementOrderStatusCacheAdd :: Text -> IO ()
-incrementOrderStatusCacheAdd merchantId =
-  Prometheus.withLabel
-    orderStatusCacheAddCounter
-    merchantId
-    Prometheus.incCounter
-
-{-# NOINLINE requestLatency #-}
-requestLatency :: Prometheus.Vector Prometheus.Label7 Prometheus.Histogram
-requestLatency =
-  Prometheus.unsafeRegister $
-  Prometheus.vector
-    ( "status_code"
-    , "method"
-    , "path"
-    , "host"
-    , "eulerInstance"
-    , "pid"
-    , "merchant_id") $
-  Prometheus.histogram info buckets
-  where
-    info =
-      Prometheus.Info
-        "euler_http_request_duration"
-        "duration histogram of http responses labeled with: status_code, method, path, host, eulerInstance, pid, merchant_id"
-    buckets = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 20, 30]
-
-observeSeconds ::
-     Maybe Text
-  -> Maybe Text
-  -> Maybe Text
-  -> Maybe Text
-  -> TimeSpec
-  -> TimeSpec
-  -> IO ()
-observeSeconds status method path merchantId start end = do
-  let latency =
-        fromRational $
-        toRational (toNanoSecs (end `diffTimeSpec` start) % 1000000000)
-  envVars <- Map.fromList <$> getEnvironment
-  let eulerInstance = T.pack <$> Map.lookup "HOSTNAME" envVars
-  let host = Nothing
-  processId <- getProcessID
-  let pid = tshow <$> Just processId
-  Prometheus.withLabel
-    requestLatency
-    ( fromMaybe "" status
-    , fromMaybe "" method
-    , fromMaybe "" path
-    , fromMaybe "" host
-    , fromMaybe "" eulerInstance
-    , fromMaybe "" pid
-    , fromMaybe "" merchantId)
-    (flip Prometheus.observe latency)
