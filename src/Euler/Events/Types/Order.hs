e8d4826026c75e5787283f4e4a3d86b252398c33
Vladislav Sabanov <vladislav.sabanov@juspay.in>
2021-03-17 17:18:37 +0500

    Update OrderStatus

--- src/Euler/Events/Types/Order.hs
+++ src/Euler/Events/Types/Order.hs
@@ -1,5 +1,13 @@
 module Euler.Events.Types.Order where
 
+{-
+This module depricated.
+Please, use euler-types and make custom type in your api.
+See src/Euler/Common/ExtraLog.hs from order-api for example.
+This module left untouched for tests.
+-}
+
+
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Text (Text)
 import Data.Time (UTCTime)
@@ -40,6 +48,7 @@ data OrderStatus
   | CAPTURE_FAILED
   | VOID_FAILED
   | AUTO_REFUNDED
+  | PARTIAL_CHARGED
   deriving (Show, Eq, Generic)
   deriving anyclass (ToJSON, FromJSON)
 
